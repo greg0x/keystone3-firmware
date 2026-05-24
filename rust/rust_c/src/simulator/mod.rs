2121ea60ee2dc6432150a3f4de128f79a6d30fef
Greg Nagy <greg@dhamma.works>
2026-05-24 18:00:16 +0200

    Enable macOS simulator screen QR scanning

--- rust/rust_c/src/simulator/mod.rs
+++ rust/rust_c/src/simulator/mod.rs
@@ -7,8 +7,10 @@ pub extern "C" fn read_qr_code_from_screen(
     on_qr_detected: extern "C" fn(*const c_char) -> bool,
     max_loop_count: u32,
 ) {
-    let _ = continuously_read_qr_code_from_screen(
+    if let Err(err) = continuously_read_qr_code_from_screen(
         |qr_code: &str| on_qr_detected(convert_c_char(qr_code.to_string())),
         max_loop_count,
-    );
+    ) {
+        eprintln!("Failed to read QR code from screen: {err}");
+    }
 }
